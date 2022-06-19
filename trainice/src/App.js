388e5f3d6cfbffc81ef85506a4a8f21e03003703
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2022-06-20 00:15:46 +0700

    Update

--- trainice/src/App.js
+++ trainice/src/App.js
@@ -1,17 +1,23 @@
-// import logo from './logo.svg';
-import HomePage from './homePage/homePage';
-import Room from './room/room';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div >
-      <div className='app'>
-        {/* <HomePage /> */}
-        <Room />
-      </div>
-
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
