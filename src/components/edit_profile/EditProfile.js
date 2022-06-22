00d15c79ef521d9106d6dcaa6e6f9213a0f39bd5
abdul aziz <azizkrian@gmail.com>
2022-06-22 09:53:29 +0700

    tambah folder fe

---
+++ src/components/edit_profile/EditProfile.js
@@ -0,0 +1,73 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import "./EditProfile.css";
+import EditProfileImage from "./assets/EditProfile/EditProfileImage.svg";
+
+function EditProfile() {
+  return (
+    <>
+      <Header></Header>
+      <div className="edit-profile-container container-fluid">
+        <div className="edit-profile-content">
+          <div className="left-container">
+            <img src={EditProfileImage} />
+          </div>
+          <div className="mid-container">
+            <div className="mid-content">
+              <h5>Gustio Nusamba</h5>
+              <p>gusti2480@gmail.com</p>
+              <form>
+                <label for="name">Nama</label>
+                <br />
+                <input type="text" id="name" name="name" />
+                <br />
+                <label for="email">Email</label>
+                <br />
+                <input type="email" id="email" name="email" />
+                <br />
+                <label for="hp">No. HP</label>
+                <br />
+                <input type="number" id="hp" name="hp" />
+              </form>
+            </div>
+          </div>
+          <div className="right-container">
+            <div className="right-content">
+              <form>
+                <p id="gender">Gender</p>
+                <input type="radio" id="laki" name="gender" value="laki" />
+                <label for="laki" class="gender-label">
+                  Laki-laki
+                </label>
+                <input
+                  type="radio"
+                  id="perempuan"
+                  name="gender"
+                  value="perempuan"
+                />
+                <label for="perempuan" class="gender-label">
+                  Perempuan
+                </label>
+                <br />
+                <label for="password" id="password-label">
+                  Password
+                </label>
+                <br />
+                <input type="password" id="password" name="password" />
+                <br />
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="form-submit">
+          <form>
+            <input type="submit" id="submit" value="Simpan Perubahan" />
+          </form>
+        </div>
+      </div>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default EditProfile;
---
+++ src/components/forgot_password/Forgot.js
@@ -0,0 +1,40 @@
+import "./Forgot.css";
+import ForgotImg from "./assets/forgot.png";
+
+function Forgot() {
+  return (
+    <div className="forgot-container container-fluid">
+      <div className="left-container">
+        <div className="left-content">
+          <h1 id="title">Trainice</h1>
+          <div className="password-title">
+            <h3>Lupa Password</h3>
+            <p>
+              Jangan khawatir! Masukkan email atau username anda di bawah dan
+              kami akan mengirimkan email dengan instruksi untuk mereset
+              password anda.
+            </p>
+          </div>
+          <p for="email" className="email">
+            Email atau Username
+          </p>
+          <form method="post">
+            <input type="email" className="email-box" name="email" required />
+            <br />
+            <input type="submit" id="submit" name="submit" value="Kirim" />
+          </form>
+          <p className="description">
+            Kembali ke <a href="#">Login</a>
+          </p>
+        </div>
+      </div>
+      <div className="right-container">
+        <div className="right-content">
+          <img src={ForgotImg} alt="Forgot Image" className="forgot-image" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Forgot;
