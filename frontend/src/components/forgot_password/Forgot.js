7be9e8eecb633ffd077e06bebdbf0e43a085139e
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2022-06-28 02:51:57 +0700

    Final Project Finalization

--- frontend/src/components/forgot_password/Forgot.js
+++ frontend/src/components/forgot_password/Forgot.js
@@ -1,7 +1,7 @@
 import "../Registrasi/Form.css";
 import ForgotImg from "./assets/forgot.png";
 import { Link } from "react-router-dom";
-import Trainice from "../assets/Trainice.png";
+import TrainiceLogo from "../assets/Trainice.png";
 
 function Forgot() {
   return (
@@ -9,23 +9,36 @@ function Forgot() {
       <div className="form-content-left col-md-6 col-12 py-5">
         <form className="form">
           <div className="form-group">
-            <img id="logo" src={Trainice} alt="Logo Trainice" />
+            <img
+              id="logo"
+              src={TrainiceLogo}
+              alt="Logo Trainice"
+              className="mb-5"
+            />
             <div className="password-title">
-              <h3>Lupa Password</h3>
-              <p>Jangan khawatir! Masukkan email atau username anda di bawah dan kami akan mengirimkan email dengan instruksi untuk mereset password anda.</p>
+              <h3 className="mb-4">Lupa Password</h3>
+              <p>
+                Jangan khawatir! Masukkan email atau username anda di bawah dan
+                kami akan mengirimkan email dengan instruksi untuk mereset
+                password anda.
+              </p>
             </div>
             <div className="form-inputs">
-              <label htmlFor="email" className="form-label">
+              <label htmlFor="email" className="form-label mb-3">
                 Email
               </label>
-              <input type="text" name="email" className="form-input form-control form-control-sm" />
+              <input
+                type="text"
+                name="email"
+                className="form-input form-control form-control-sm"
+              />
             </div>
             <button type="submit" id="signup" name="submit">
               Kirim
             </button>
             <p className="form-input-login text-center">
               Kembali ke{" "}
-              <Link to="/login" className="daftar">
+              <Link to="/login" className="daftar fw-bold">
                 Login
               </Link>
             </p>
