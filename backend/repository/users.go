638694bce9dd4190eb4b873dc8173a574672ce18
Syifa Nur Aini <59914267+syifanuraini24@users.noreply.github.com>
2022-06-27 21:36:46 +0700

    update get profile by email

--- backend/repository/users.go
+++ backend/repository/users.go
@@ -100,9 +100,9 @@ func (p *UserRepository) GetProfile(email string) (User, error) {
 
 	return users, nil
 }
-func (p *UserRepository) Dashboard(id int64) (User, error) {
+func (p *UserRepository) Dashboard(email string) (User, error) {
 	var users User
-	err := p.db.QueryRow("SELECT * FROM users WHERE id = ?", id).Scan(&users.ID, &users.Nama, &users.Email, &users.Password, &users.Gender, &users.No_hp)
+	err := p.db.QueryRow("SELECT * FROM users WHERE email = ?", email).Scan(&users.ID, &users.Nama, &users.Email, &users.Password, &users.Gender, &users.No_hp)
 	if err != nil {
 		return users, err
 	}
